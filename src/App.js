9df6f316e33dd75e86c60d53d59528cc5d5a0804
Asad <mdasadansari123@gmail.com>
2021-12-04 12:36:02 +0530

    Trying commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+const App = ()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let newTime = new Date().toLocaleTimeString();
+const[currTime,changeTime] = useState(newTime);
+
+const newTi = ()=>{
+  newTime = new Date().toLocaleTimeString();
+  changeTime(newTime)
 }
+setInterval(newTi,1000)
 
+  return (
+  <>
+    <h1> {currTime} </h1>
+    {/* <button onClick={newTi}>Change Time</button> */}
+  </>
+  )
+}
 export default App;
